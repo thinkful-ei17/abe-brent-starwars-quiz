79c0ecf0175ca1bede4ff8c08c61a48439e79de6
Brent Guistwite <BrentGuistwite@gmail.com>
2017-12-12 21:27:44 -0500

    in the process of adding comments to the code

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 'use strict';
-
+//STORE holds data about the current state
+const STORE = {
+  view: 'start',
+  currentQuestion: -1,
+  currentScore: 0,
+  userAnswer: []
+};
+//These are the questions and answers for the quiz
 const questions = [
   {
     question: 'Who is Luke Skywalker\'s Dad?',
@@ -60,15 +67,187 @@ const questions = [
     correctAnswer: 5
   }
 ];
+//These are the different result outcomes for the quiz
+const character = [
+  {
+    'message': 'Oh! Aw! Ooh! Uh! Ai, Ai! Whoa, Ai! Whaaa! What? Wha- Oh.',
+    'image': './img/jarjarbinks.jpg',
+    'alt': 'Jar-Jar Binks'
+  },
+  {
+    'message': 'Do or do not, there is no try...',
+    'image': './img/youngling.jpg',
+    'alt': 'Youngling'
+  },
+  {
+    'message': 'Patience you must have my young Padawan.',
+    'image': './img/padawan.jpg',
+    'alt': 'Padawan'
+  },
+  {
+    'message': 'Ignorance, yet Knowledge.',
+    'image': './img/jediknight.jpg',
+    'alt': 'Jedi Knight'
+  },
+  {
+    'message': 'Strong with you , the force is',
+    'image': './img/jedimaster.png',
+    'alt': 'Jedi Master'
+  },
+  {
+    'message': 'The Force will be with you. Always.',
+    'image': './img/jedigrandmaster.png',
+    'alt': 'Jedi Grand Master'
+  },
+];
 
-const STORE = {
-  view: 'start',
-  currentQuestion: -1,
-  currentScore: 0,
-  userAnswer: []
-};
+//
+function handleStartClicked() {
+  $('.intro').submit(function (event) {
+    event.preventDefault();
+    STORE.view = 'quiz';
+    const firstQuestion = UpdateQuestion();
+    renderQuiz();
+  });
+}
 
-function renderView() {
+//
+function handleAnswerClicked() {
+  $('.quiz').submit(function (event) {
+    event.preventDefault();
+    generateNextButton();
+    $('.answer-button').remove();
+    $('label').toggleClass('incorrect');
+    $(`label[for=${questions[STORE.currentQuestion].correctAnswer}]`).removeClass('incorrect');
+    if (checkAnswer()) {
+      scoreKeeper();
+      generateScore();
+    }
+  });
+}
+
+//
+function handleNextClicked() {
+  $('.quiz').on('click', '.next-button', function (event) {
+    event.preventDefault();
+    let nextQuestion = UpdateQuestion();
+    if (nextQuestion !== null) {
+      renderQuiz();
+    } else {
+      STORE.view = 'Status';
+      renderQuiz();
+      generateStatus();
+    }
+  });
+}
+
+//
+function generateIntro() {
+  $('.intro').html(
+    `<p>In a land far, far away...</p>
+<form>
+  <input class="start-button button" type="submit" value="Start Quiz">
+</form>`
+  );
+}
+
+//
+function generateQuiz() {
+  $('.quiz').html(`<span class = 'question-number'></span>
+  <form id="questions">
+    <p class="question"></p>
+      <input id='answer' class="answer-button button" type="submit" value="Answer">
+    </form>
+    
+    <p>Quiz Progress</p>
+    <span class="progress"></span>
+
+    <span class="current-score"></span>`);
+}
+
+//generates the question number to be displayed as "Q{number of question}"
+function generateQuestionNumber() {
+  $('.question-number').html(`
+  Q${STORE.currentQuestion + 1}
+  `);
+}
+
+//generates a string of html with the current question
+function generateQuestion() {
+  $('.question').html(`
+  ${questions[STORE.currentQuestion].question}<br>
+  `);
+}
+
+//generates a string of html with answers
+function generateAnswer() {
+  let answerHtml = '';
+  for (let i = 0; i < questions[STORE.currentQuestion].answers.length; i++) {
+    answerHtml += `<input type="radio" value="${questions[STORE.currentQuestion].answers[i].id}" id="${i}" name="answer" required>
+    <label for="${questions[STORE.currentQuestion].answers[i].id}">
+    ${questions[STORE.currentQuestion].answers[i].answer}</label><br>
+  `;}
+  $('.question').append(answerHtml); //<<<<<<<<ANOTHER FUNCTION???
+}
+
+//
+function generateNextButton() {
+  $(`<form>
+  <input id='next' class="next-button button" type="button" value="Next">
+</form>`).insertAfter('.question');
+}
+
+
+//generate a string of html with current score
+function generateScore() {
+  $('.current-score').html(`
+    ${STORE.currentScore}/${questions.length}
+  `);
+}
+
+//
+function generateStatus() {
+  const msg = character[STORE.currentScore].message;
+  const img = character[STORE.currentScore].image;
+  const alt = character[STORE.currentScore].alt;
+  $('.status').html(`
+    <h1>Score: ${STORE.currentScore}</h1>
+    <h2 class="score-message">${STORE.currentScore === 0 ? 'Yousa' : 'You\'re a'} ${alt} </h2>
+    <img src="${img}" alt='${alt}' class="score-image">
+    <p>&ldquo;${msg}&rdquo;</p>
+    <p>Think you can do better?</p>
+    <form>
+      <input class="try-again-button button" type="submit" value="Try Again">
+    </form>
+    `);
+}
+
+//
+function UpdateQuestion() {
+  if (STORE.currentQuestion < questions.length - 1) {
+    STORE.currentQuestion++;
+    return STORE.currentQuestion;
+  } else {
+    return null;
+  }
+} 
+
+//
+function checkAnswer() {
+  // Compare id of the selected answer with the id of the correct answer
+  // only return on true
+  const userAnswer = $('input[name=answer]:checked').val();
+  STORE.userAnswer.push(userAnswer);
+  return parseInt(userAnswer) === questions[STORE.currentQuestion].correctAnswer;
+}
+
+//Increments score count based on number of correct answers
+function scoreKeeper() {
+  STORE.currentScore++;
+}
+
+//
+function viewChecker() {
   if (STORE.view === 'start') {
     $('.intro').show();
     $('.quiz').hide();
@@ -85,215 +264,26 @@ function renderView() {
     $('.answers').hide();
     $('.status').show();
     $('.quiz').hide();
-  } 
-}
-
-function generateIntro() {
-  $('.intro').html(
-    `<p>In a land far, far away...</p>
-<form>
-  <input class="start-button button" type="submit" value="Start Quiz">
-</form>`
-  );
-}
-
-const character = [
-   { "message": "Oh! Aw! Ooh! Uh! Ai, Ai! Whoa, Ai! Whaaa! What? Wha- Oh.",
-  "image": "./img/jarjarbinks.jpg",
-  "alt":"Jar-Jar Binks"
-  },
-  { "message": "Do or do not, there is no try...",
-  "image": "./img/youngling.jpg",
-  "alt":"Youngling"
-  },
-  { "message": "Patience you must have my young Padawan.",
-  "image": "./img/padawan.jpg",
-  "alt":"Padawan"
-  },
-  { "message": "Ignorance, yet Knowledge.",
-  "image": "./img/jediknight.jpg",
-  "alt":"Jedi Knight"
-  },
-  { "message": "Strong with you , the force is",
-  "image": "./img/jedimaster.png",
-  "alt": "Jedi Master"
-  },
-  { "message": "The Force will be with you. Always.",
-  "image": "./img/jedigrandmaster.png",
-  "alt":"Jedi Grand Master"
-  },
-]
-
-function generateStatus(){
-  const msg = character[STORE.currentScore].message;
-  const img = character[STORE.currentScore].image;
-  const alt = character[STORE.currentScore].alt;
-  $('.status').html(
-    `
-    <h1>Score: ${STORE.currentScore}</h1>
-    <h2 class="score-message">${STORE.currentScore === 0 ? 'Yousa' : 'You\'re a' } ${alt} </h2>
-    <img src="${img}" alt='${alt}' class="score-image">
-    <p>&ldquo;${msg}&rdquo;</p>
-    <p>Think you can do better?</p>
-    <form>
-      <input class="try-again-button button" type="submit" value="Try Again">
-    </form>
-    `
-
-  );
-}
-
-
-function UpdateQuestion() {
-  if (STORE.currentQuestion < questions.length-1) {
-    STORE.currentQuestion++;
-    return STORE.currentQuestion;
-  } else {
-    return null;
   }
 }
-//Increments score count based on number of correct answers
-function scoreKeeper() {
-  STORE.currentScore++;
-  console.log('score keeper ran');
-}
-//generate a string of html with current score
-function generateScore() {
-  $('.current-score').html(`
-    ${STORE.currentScore}/${questions.length}
-  `);
-  console.log('generate score ran');
-}
-//generates a string of html with answers
-function generateAnswer() {
-  let answerHtml = '';
-  for (let i = 0; i < questions[STORE.currentQuestion].answers.length; i++) {
-    answerHtml += `<input type="radio" value="${questions[STORE.currentQuestion].answers[i].id}" id="${i}" name="answer" required>
-    <label for="${questions[STORE.currentQuestion].answers[i].id}">${
-  questions[STORE.currentQuestion].answers[i].answer
-}</label><br>`;
-  }
-  $('.question').append(answerHtml);
-  console.log('generate answer ran');
-}
-//generate a string of html with question
-function generateQuestion() {
-  $('.question').html(`
-  ${questions[STORE.currentQuestion].question}<br>
-  `);
-  console.log('generate question ran');
-  
-}
 
-function generateQuestionNumber(){
-  $('.question-number').html(`
-  Q${STORE.currentQuestion + 1}
-  `);
-  console.log('generating q number');
-}
-
-function template() {
-  $('.quiz').html(`<span class = 'question-number'></span>
-  <form id="questions">
-    <p class="question"></p>
-      <input id='answer' class="answer-button button" type="submit" value="Answer">
-    </form>
-    
-    <p>Quiz Progress</p>
-    <span class="progress"></span>
-
-    <span class="current-score"></span>`);
-  console.log('template ran');
-}
-
+//This function is responsible for rendering the page
 function renderQuiz() {
-  template();
+  generateQuiz();
   generateQuestion();
   generateQuestionNumber();
   generateAnswer();
   generateScore();
-  renderView();
-}
-
-// function displayAnswer() {
-//   $('.quiz label').addClass('incorrect');
-// }
-
-
-function checkAnswer() {
-  // Compare id of the selected answer with the id of the correct answer
-  // only return on true
-
-
-  const userAnswer = $('input[name=answer]:checked').val();
-  STORE.userAnswer.push(userAnswer);
-
-  return parseInt(userAnswer) === questions[STORE.currentQuestion].correctAnswer;
-}
-
-function generateAnswerHtml(){
-  $(`<form>
-  <input id='next' class="next-button button" type="button" value="Next">
-</form>`).insertAfter('.question');
-    
-  
-  console.log('generate answer html ran')
-  ;}
-
-function handleAnswerClicked(){
-  $('.quiz').submit(function(event){
-    event.preventDefault();
-    console.log('answer clicked'); 
-    generateAnswerHtml();
-    $('.answer-button').remove();
-    $('label').toggleClass('incorrect');
-    $(`label[for=${questions[STORE.currentQuestion].correctAnswer}]`).removeClass('incorrect');
-    if (checkAnswer()) {
-      scoreKeeper();
-      generateScore();
-    }
- 
-   
- 
-  });
-  
-}
-
-
-function handleNextClicked() {
-  // Push the selected to userAnswer array
-  $('.quiz').on('click','.next-button',function(event) {
-    event.preventDefault();
-    console.log('HEY IM WORKING!!!')
-  
-
-    let nextQuestion = UpdateQuestion();
-    if (nextQuestion !== null) {
-      renderQuiz();
-    } else {
-      STORE.view = 'Status';
-      renderQuiz();
-      generateStatus();
-    }
-  });
-}
-
-function handleStartClicked() {
-  $('.intro').submit(function(event) {
-    event.preventDefault();
-    STORE.view = 'quiz';
-    const firstQuestion = UpdateQuestion();
-    renderQuiz();
-  });
+  viewChecker();
 }
 
 // Render State
 function main() {
   generateIntro();
-  renderView();
+  viewChecker();
   handleStartClicked();
   handleAnswerClicked();
   handleNextClicked();
 }
-
+//
 $(main);
